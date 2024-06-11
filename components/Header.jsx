1de48a21ec9a354803ec55568b82cf74a09028e5
shubhammgore <shubh.shubham21@gmail.com>
2024-06-11 18:20:54 +0530

    add Header Nav and all routes for all pages and intall framer motion page transition

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Components
+import Nav from "./Nav";
+
 const Header = () => {
-  return <header>Header</header>;
+    return (
+        <header className='py-8 xl:py-12 text-white'>
+            <div className='container mx-auto flex justify-between items-center'>
+                {/* Logo */}
+                <Link href='/'>
+                    <h1 className='text-4xl font-semibold'>
+                        Shubham<span className='text-accent'>.</span>
+                    </h1>
+                </Link>
+                {/* Desktop Nav & hire me button*/}
+                <div className='hidden xl:flex items-center gap-8'>
+                    <Nav />
+                    <Link href='/contact'>
+                        <Button>Hire me </Button>
+                    </Link>
+                </div>
+
+                {/* Mobile nav */}
+                <div className='xl:hidden'>mobile nav</div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
